7c4b0b83fecc445e196c4661011b9f4c3394bd34
Tychool <ok.hezekiahs@gmail.com>
2023-11-07 11:27:08 +0100

    Updated header f file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_HEADER
+#define SHELL_HEADER
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+#include <stdlib.h>
+
+
+#endif
