3f68796c291e771635e4562385729886ee31473b
Hezekiahs <111096175+Tychool@users.noreply.github.com>
2023-11-17 09:21:15 +0100

    Update 0-handle_enviroment.c

--- 0-handle_enviroment.c
+++ 0-handle_enviroment.c
@@ -88,4 +88,21 @@ int th_unset_env(data_t *data, char *variable)
 	return (data->env_ch);
 }
 
+#include "shell.h"
 
+/**
+ * th_env_del - delete env var
+ * @data: data
+ * Return: 0
+ */
+int th_env_del(data_t *data)
+{
+	if (data->arg_count == 1)
+	{
+		th_errouts(ARG_ERROR0);
+		return (1);
+	}
+	for (int i = 1; i < data->arg_count; i++)
+		th_unset_env(data, data->arg_vector[i]);
+	return(0);
+}
