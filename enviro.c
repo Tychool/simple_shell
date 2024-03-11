b7d2b9665d0c9917549593c6b9b66eebcec45431
Hezekiahs <111096175+Tychool@users.noreply.github.com>
2024-03-12 00:13:56 +0100

    Update enviro.c

--- enviro.c
+++ enviro.c
@@ -101,3 +101,37 @@ void th_unsetenv(char **av)
         }
     }
 }
+
+/**
+ * th_getenv - get global variable
+ * @name: var name
+ * Return: var value (strings)
+ */
+char *th_getenv(const char *name)
+{
+    int i, j;
+    char *value;
+
+    if (!name)
+        return (NULL);
+    for (i = 0; environ[i]; i++)
+    {
+        j = 0;
+        if (name[j] == environ[i][j])
+        {
+            while (name[j])
+            {
+                if (name[j] != environ[i][j])
+                    break;
+
+                j++;
+            }
+            if (name[j] == '\0')
+            {
+                value = (environ[i] + j + 1);
+                return (value);
+            }
+        }
+    }
+    return (0);
+}
