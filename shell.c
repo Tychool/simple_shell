bffc17108ef3027df757757c9d664c547947e480
Hezekiahs <111096175+Tychool@users.noreply.github.com>
2024-03-12 01:53:07 +0100

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,43 @@
+int main(void)
+{
+	ssize_t len = 0;
+  size_t size = 0;
+	thlist_path *head = '\0';
+  char *buff = NULL, *value, *path_name, **av;
+	void (*fun_sh)(char **);
+
+	signal(SIGINT, th_sig_handler);
+	while (len != EOF)
+	{
+		th_isatty();
+		len = getline(&buff, &size, stdin);
+		th_EOF(len, buff);
+		av = th_splitstring(buff, " \n");
+		if (!av || !av[0])
+			execute(av);
+		else
+		{
+			value = th_getenv("PATH");
+			head = th_linkpath(value);
+			path_name = th_which(arv[0], head);
+			fun_sh = th_checkbuild(av);
+			if (f)
+			{
+				free(buff);
+				fun_sh(av);
+			}
+			else if (!path_name)
+				execute(av);
+			else if (path_name)
+			{
+				free(av[0]);
+				av[0] = path_name;
+				execute(av);
+			}
+		}
+	}
+	th_free_list(head);
+	th_free_av(av);
+	free(buff);
+	return (0);
+}
